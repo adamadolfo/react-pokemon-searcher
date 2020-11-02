57746d4d964c9b01d0dc51bee1019ba30b40bd31
Adam <adamadolfo8@gmail.com>
2020-11-02 17:02:03 -0500

    half done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,31 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+state = {
+  sprite: true
+}
+
+toggleSprite = () => {
+  this.setState({
+    sprite: !this.state.sprite
+  })
+}
+
   render() {
     return (
-      <Card>
+      <Card onClick={this.toggleSprite}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            {this.state.sprite ? <img src={this.props.pokemon.sprites.front} /> : <img src={this.props.pokemon.sprites.back} />} 
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+    <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemon.hp}
             </span>
           </div>
         </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,43 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemon: [],
+    filter: ""
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+    .then(r => r.json())
+    .then(data => this.setState({
+      pokemon: data
+    }))
+  }
+  
+
+  handleSearch = (input) => {
+    this.setState({
+      filter: input
+    })
+    combine()
+  }
+
+  
+
   render() {
+    let combine = () => {
+    return this.state.pokemon.filter(pokemon => pokemon.name.includes(this.state.filter))
+    }
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
         <PokemonForm />
         <br />
-        <Search />
+        <Search handleSearch={this.handleSearch}/>
         <br />
-        <PokemonCollection />
+        <PokemonCollection allPokes={combine()} />
       </Container>
     )
   }
